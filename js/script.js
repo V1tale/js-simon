5344e9bebb4ac7fd2ff1e7f4dd9aa4e6332a10bd
V1tale <elativ.c@gmail.com>
2022-10-20 16:20:13 +0200

    completata logica js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-// CONSEGNA: Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri spariscono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
 // CREO 5 NUMERI CASUALI TRAMITE FUNZIONE
+// INITIALIZING
 let numbersArray = [];
 for(i=0;i < 5; i++) {
     let numbers = randomNumber(1, 100);
@@ -11,28 +8,29 @@ for(i=0;i < 5; i++) {
 }
 console.log(numbersArray);
 // FACCIO APPARIRE I NUMERI IN PAGINA
-let title = document.getElementById("randomNumbers");
+const title = document.getElementById("randomNumbers");
 title.append(numbersArray);
 // IMPOSTO UN TIMER DI 30 SECONDI
 // FACCIO SPARIRE I NUMERI DALLA PAGINA
-let timer = setTimeout(cleaner, 2000);
+let timer = setTimeout(cleaner, 3000);
+// INPUT
 // L'UTENDE DEVE INSERIRE I NUMERI TRAMITE PROMPT
-let userNumbers = [];
-let input = setTimeout(prompts, 2000);
-console.log(userNumbers);
-// CONFRONTO I NUMERI DIGITATI CON QUELLI APPARSI IN PRECEDENZA
-let validNumbers = [];
-for(i = 0; i < 5; i++) {
-    if (numbersArray.includes(userNumbers[i])) {
-        validNumbers.push(userNumbers[i]);
-    }
-}
-console.log(validNumbers);
-// COMUNICO QUANTI DI QUESTI NUMERI SONO GIUSTI
+setTimeout( function () {
+    let userNumbers = prompts(5);
+    console.log(userNumbers);
+    // PROCESSING
+    // CONFRONTO I NUMERI DIGITATI CON QUELLI APPARSI IN PRECEDENZA
+    // let validNumbers = compareArrays(numbersArray, userNumbers)
+    let validNumbers = CompareArrays(numbersArray, userNumbers);
+    console.log(validNumbers);
+    // OUTPUT
+    // COMUNICO QUANTI DI QUESTI NUMERI SONO GIUSTI
+    let resultString = document.getElementById("result")
+    resultString.innerHTML = `Ne hai indovinate: ${validNumbers.length}`;
+    let rightNumbers = document.getElementById("valid-numbers");
+    rightNumbers.innerHTML = `I numeri giusti: ${validNumbers}`
 
-
-
-
+}, 3000);
 
 
 // FUNCTIONS
@@ -44,15 +42,22 @@ return (Math.floor(Math.random() * (max - min) + min));
 function cleaner(string) {
     title.classList.add("hidden");
 }
-
-function prompts(){
+function prompts() {
+    let userNumbersResult = [];
     for(i=0; i<5; i++) {
         userNumArray = prompt("inserisci un numero")
         userNum = parseInt(userNumArray);
-
-        userNumbers.push(userNum);
+        userNumbersResult.push(userNum);
     }
-
-    
-    return userNumbers;
+    return userNumbersResult;
+}
+function CompareArrays(arrayOne, arrayTwo) {
+    const result = [];
+    for (let i = 0; i < arrayOne.length; i++) {
+        const thisNumber = arrayOne[i];
+        if (arrayTwo.includes(thisNumber)) {
+            result.push(thisNumber);
+        }
+    }
+    return result;
 }
